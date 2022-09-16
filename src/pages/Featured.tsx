0dde1551c714109d713f9b98208af06135b3c8d6
Hana Nabilah Fatinah <56430915+raihanahnabilah@users.noreply.github.com>
2022-09-16 10:20:38 +0700

    Update Featured page and push build files

--- src/pages/Featured.tsx
+++ src/pages/Featured.tsx
@@ -29,6 +29,36 @@ const Featured = () => {
                 description="“Wahana Anak Bangsa Mengejar Asa”"
                 link="https://koran-jakarta.com/wahana-anak-bangsa-mengejar-asa?page=all"
             />
+            <LinkModal
+                title="Britzone"
+                description="“What your English teacher did not tell you (with Hana N. Fatinah)”"
+                link="https://www.instagram.com/p/CECHwyGsLvG/"
+            />
+            <LinkModal
+                title="KIR at SMAN 90 Jakarta"
+                description="Workshop on applying for scholarships and studying abroad."
+                link="https://www.instagram.com/stories/highlights/17874575911330813/"
+            />
+            <LinkModal
+                title="Yale-NUS College"
+                description="Recess Week Work at Yale-NUS Student Services."
+                link="https://www.linkedin.com/feed/update/urn:li:activity:6643787174334521345/"
+            />
+            <LinkModal
+                title="Yale-NUS College"
+                description="Winning Case Competition at LTA x NUS Economics."
+                link="https://www.facebook.com/yalenuscollege/posts/pfbid02JYXQUDjCk2htknaevQe5E2wkXH633fh11mCmcEa4zkx6tYYWKYA4EMK2Y9EnGtsRl"
+            />
+            <LinkModal
+                title="Podcast Pemuda Selatan"
+                description="“Kehidupan Lockdown di Negara Tetangga (ft. Raihanah Nabilah Fatinah)”"
+                link="https://open.spotify.com/episode/6fJjNN6jgXKYH30WCqpWnY?si=90f30ea55d8a472f"
+            />
+            <LinkModal
+                title="Yale-NUS College"
+                description="Talking about my experience performing at Bread and Jam 2022 with Arkipelago band."
+                link="https://www.yale-nus.edu.sg/story/24-dec-2019-on-campus-music-events-give-students-opportunities-to-showcase-talent/"
+            />
         </Box>
     );
 }
